d78ff1351223442c0b3e8b3832747546c1595a3b
Maximilian Kasy <maxkasy@gmail.com>
2024-04-01 20:53:11 -0400

    diminished chords

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# jazz_flash_card
- Flashcards for jazz practice
+# Flashcards for jazz practice
+
+This Python Dash app is deployed at [https://jazz-flash-card.onrender.com/](https://jazz-flash-card.onrender.com/).
--- application.py
+++ application.py
@@ -11,32 +11,40 @@ server = app.server
 app.layout = html.Div([
     dcc.Checklist(
         id='exercise-selection',
-        options=[{'label': " " + i, 'value': i} for i in ["Arpeggio", "Chords", "Scale"]],
-        value=["Arpeggio", "Chords", "Scale"],
+        options=[{'label': " " + i, 'value': i} for i in ["Arpeggios", "Chords", "Scales"]],
+        value=["Arpeggios", "Chords", "Scales"],
+        inline=True,
+        labelStyle={'margin-right': '20px'}
+    ),    
+    html.Div(style={'height': '25px'}),
+    dcc.Checklist(
+        id='scale-selection',
+        options=[{'label': " " + i, 'value': i} for i in ["Δ", "-7", "7", "ii-V7-I", "dim7"]],
+        value=["Δ", "-7", "7", "ii-V7-I", "dim7"],
         inline=True,
         labelStyle={'margin-right': '20px'}
     ),
     html.Div(style={'height': '50px'}),
     html.Button('Next', id='next-button', n_clicks=0),
     html.Div(style={'height': '50px'}),
-    html.H1(id='flashcard-display', children='Press Next to start', style={'fontSize': '100px'}),
+    html.H1(id='flashcard-display', children='Press Next to start', style={'fontSize': '80px'}),
 ], style={'margin': '5% auto', 'maxWidth': '800px', 'textAlign': 'center'}, id='app-layout')
 
 @app.callback(
     Output('flashcard-display', 'children'),
     [Input('next-button', 'n_clicks'),
-     Input('exercise-selection', 'value')]
+     Input('exercise-selection', 'value'),
+     Input('scale-selection', 'value')]
 )
-def update_output(n_clicks, selected_exercises):
+def update_output(n_clicks, selected_exercises, selected_scales):
     if n_clicks > 0:
-        return flashcard(selected_exercises)
+        return flashcard(selected_exercises, selected_scales)
     else:
         return 'Press Next to start'
 
-def flashcard(selected_exercises):
+def flashcard(selected_exercises, selected_scales):
     keys = ["A", "B♭", "B", "C", "C♯", "D", "E♭", "E", "F", "F♯", "G", "A♭"]
-    scales = ["Δ", "-7", "7", "ii-V7-I"]
-    return random.choice(keys) + random.choice(scales) + ": " + random.choice(selected_exercises)
+    return random.choice(keys) + random.choice(selected_scales) + ": " + random.choice(selected_exercises)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
